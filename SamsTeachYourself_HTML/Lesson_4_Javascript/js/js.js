0974f0dc036a62c0b14c4c6060df1decd2fef3d5
mistertoi <94171481+mistertoi@users.noreply.github.com>
2023-04-16 20:02:12 +0200

    lesson_5 debugging

--- SamsTeachYourself_HTML/Lesson_4_Javascript/js/js.js
+++ SamsTeachYourself_HTML/Lesson_4_Javascript/js/js.js
@@ -1,1 +1,21 @@
-document.write(document.lastModified);
+function workedon() {
+    document.write(document.lastModified);
+}
+
+function clocktime() {
+    now = new Date();
+    localtime = now.toString();
+    utctime = now.toUTCString();
+    document.write("<p><strong>Local time:</strong> " + localtime + "</p>");
+    document.write("<p><strong>UTC time:</strong> " + utctime + "</p>");
+}
+
+function watchface() {
+    hours = now.getHours();
+    mins = now.getMinutes();
+    secs = now.getSeconds();
+    msecs = now.getMilliseconds();
+    document.write("<h2>");
+    document.write(hours + ":" + mins + ":" + secs + ":" + msecs);
+    document.write("</h2>");
+}
